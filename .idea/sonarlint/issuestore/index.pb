5cfaa944f341174b447e05aa4851cefaf78ae013
Fernanda Tovar <tovfer17@gmail.com>
2021-02-22 01:44:41 -0500

    new changes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,19 +1,35 @@
 
-;
-venv/app.py,f/1/f17bcb9327f0ad21c86e684fa3574235c928f92d
-=
-venv/test.csv,2/5/255099a5a61c4e7f9ddc107b0b159acac88b1947
-<
-venv/LICENSE,f/b/fb395a553e66208b112079919b93028375cb0255
->
-venv/matrix.py,0/7/075913ea0cdaaaf49213aefd72254575cb6f405c
-?
-venv/maxflow.py,f/5/f571d1c2dd3a2cd529ec8e2a390b6dd41b7112a8
-G
-venv/maxflownetworkx.py,6/7/67f6a3b28f4bb9d9c74d0ff28e0bf451ffd4178f
->
-venv/edge1.csv,5/f/5f1e5345c18e84bb70e7fd7b9a60f4e793c827d1
->
-venv/node1.csv,1/2/121eab459b9b597493f02af769b4d166dd0767a4
 S
 #.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+@
+venv/__init__.py,9/f/9ffc920be63285f69158545cd7557c8c2e4e451b
+L
+venv/example #1 /__init__.py,8/b/8b4a4f24d74acbd8f53909521c59b85c70476adc
+J
+venv/example #1 /edge1.csv,3/d/3dc78346d873cbbda0dfec73d1b6a5b878db1a2b
+H
+venv/example #1 /LICENSE,2/4/2448ecadc551369ca86a873f8b6407fea7121f71
+J
+venv/example #1 /node1.csv,7/e/7e9cb6393fa32c4655d7a51509fd01947746efc9
+J
+venv/example #1 /README.md,2/7/276b98477b1f162d73334ae9ba2bdba027495781
+K
+venv/example #2/__init__.py,9/b/9bf19f87cd5dd372f707038aeb01e28dde6d7d6e
+K
+venv/example #3/__init__.py,8/1/8113286711fae8f7671a3975583fc794f1e96e65
+H
+venv/example #3/test.csv,4/9/493a7e50c1800c061d56e74d5db7a56a78b7d3e3
+K
+venv/example #4/__init__.py,3/4/345cfbb95e172748e553c0eb8dc00ec84a390699
+?
+FolderBreakdown,a/4/a4b75738448d7db8bf2c4318e370ac21d99d863f
+I
+venv/example #3/matrix.py,8/3/83515322bd37bcaa10cd1e2468e03ef37abc26e2
+K
+venv/example #2/networks.py,9/b/9b1adb2f59483716977390c173ad985d08b43d0a
+F
+venv/example #5/try.py,b/6/b65a4faf4cccd01714361b830edea53852818b0f
+J
+venv/example #4/maxflow.py,6/4/6446a28c596851dae9b8e4d596b842448997eab1
+G
+venv/example #1 /app.py,3/e/3e01f3c0c745bf0791e5ae3e41836305d0137fea
---
+++ venv/example #5/try.py
@@ -0,0 +1,106 @@
+
+import networkx as nx
+# importing matplotlib.pyplot
+import matplotlib.pyplot as plt
+import gurobipy as grb
+
+
+
+#https://networkx.org/documentation/stable/reference/classes/digraph.html
+G = nx.DiGraph()
+H = nx.path_graph(6)
+G.add_nodes_from(H)
+G.add_edges_from([(0, 1), (1, 3),(3,5),(4,5),(4,3),(3,2),(2,4),(4,1),(0,2)])
+
+
+print("Total number of nodes: ", int(G.number_of_nodes()))
+print("Total number of edges: ", int(G.number_of_edges()))
+print("List of all nodes: ", list(G.nodes()))
+print("List of all edges: ", list(G.edges(data=True)))
+print("Degree for all nodes: ", dict(G.degree()))
+print("Total number of self-loops: ", (nx.selfloop_edges(G)))
+print("List of all nodes with self-loops: ", list(nx.nodes_with_selfloops(G)))
+
+
+# https://networkx.org/documentation/stable//reference/generated/networkx.classes.function.get_node_attributes.html
+G.add_node(0 ,inflow ="0")
+G.add_node(1 ,inflow ="1000")
+G.add_node(2,inflow ="4700")
+G.add_node(3,inflow ="5000")
+G.add_node(4,inflow ="5400")
+G.add_node(5,inflow ="7700")
+print("list of inflows: ", list(G.nodes(data=True)))
+
+nodes_h = nx.get_node_attributes(G, 'inflow')
+nodes = G.nodes()
+
+# N is # of commodities and its based on the length of the inflow list so 6 commodities
+N = 0
+
+#for each node, have a list of N commodities length attached to give[][]
+ #index feature for i nodes and h commodities
+
+for i in nodes_h:
+    count = 0
+    j= nodes_h[i]
+    print("i: ", i)
+    print("j: ", j)
+    for k in range(len(j)):
+        print( nodes_h[i][k])
+
+    if N == 0:
+         N = len(j)
+
+#for each arc i,j add index h for N commodities as third index of tuple
+#list = N * number arcs in length
+
+larcs = G.edges()
+arcs_h = []
+
+arcs = tuple(G.edges())
+for i in range(len(larcs)):
+    for k in range(N):
+        ik = list(larcs[i])
+        ik.extend([k])
+        arcs_h.append(tuple(ik))
+
+arcs_h = tuple(arcs_h)
+
+supply ={}
+demand={}
+
+#remove super source/sink nodes 
+
+inflow =nx.get_node_attributes(G,'inflow')
+
+#loop through attribute data
+for i,j in inflow:
+    for k in range(len(j)):
+        if j[k]>0:
+            supply[(i,k)]=j[k]
+            nodes_h.pop(i)
+            break
+        if j[k]<0:
+            demand[(i,k)] = -j[k]
+            nodes_h.pop(i)
+            break
+
+#seperate capacity for arc specific and link capacity
+
+capacity_h={}
+capacity_arc={}
+
+#k index for commodities
+c=nx.get_edge_attributes(G,'capacity')
+
+#extract capacity from networkx graph
+for i,j in c:
+    for k in range(len(j)):
+        if k < N:
+            x=tuple([k],)
+            capacity_h[i +x]= j[k]
+        else:
+            capacity_arc[i]=j[k]
+
+#create optimization model
+m= Model('netflow')
